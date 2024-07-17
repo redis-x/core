6c385e05bb722d7d8bd71751694db51831ff5f20
Daniil Kirichenko <daniil@kirick.me>
2024-07-17 17:15:03 +0300

    PEXPIRE command

--- src/client.ts
+++ src/client.ts
@@ -4,6 +4,8 @@ import * as CommandSet from './commands/string/set';
 import { SetOptionsWithGet, SetOptions } from './commands/string/set';
 import * as CommandZrange from './commands/zset/zrange';
 import { ZrangeOptionsWithWithscores, ZrangeOptions } from './commands/zset/zrange';
+import * as CommandPexpire from './commands/keyspace/pexpire';
+import { PexpireOptions } from './commands/keyspace/pexpire';
 import * as CommandExpire from './commands/keyspace/expire';
 import { ExpireOptions } from './commands/keyspace/expire';
 import * as CommandDel from './commands/keyspace/del';
@@ -93,6 +95,19 @@ export class RedisXClient extends RedisXClientBase {
 	async ZRANGE(key: string, start: number | string, stop: number | string): Promise<string[]>;
 	async ZRANGE(...args: unknown[]): Promise<unknown> {
 		return this._sendModuleCommand(CommandZrange, args);
+	}
+	/**
+	 * This command works exactly like EXPIRE but the time to live of the key is specified in milliseconds instead of seconds.
+	 * - Available since: 2.6.0.
+	 * - Time complexity: O(1).
+	 * @param key Key to get.
+	 * @param seconds Time to live in seconds.
+	 * @param options Options. See ExpireOptionsJsdoc.
+	 * @returns Returns `1` if the timeout was set. Returns `0` if the timeout was not set; for example, the key doesn't exist, or the operation was skipped because of the provided arguments.
+	 */
+	async PEXPIRE(key: string, seconds: number, options?: PexpireOptions): Promise<0 | 1>;
+	async PEXPIRE(...args: unknown[]): Promise<unknown> {
+		return this._sendModuleCommand(CommandPexpire, args);
 	}
 	/**
 	 * Set a timeout on key.
--- src/transaction.ts
+++ src/transaction.ts
@@ -4,6 +4,8 @@ import * as CommandSet from './commands/string/set';
 import { SetOptionsWithGet, SetOptions } from './commands/string/set';
 import * as CommandZrange from './commands/zset/zrange';
 import { ZrangeOptionsWithWithscores, ZrangeOptions } from './commands/zset/zrange';
+import * as CommandPexpire from './commands/keyspace/pexpire';
+import { PexpireOptions } from './commands/keyspace/pexpire';
 import * as CommandExpire from './commands/keyspace/expire';
 import { ExpireOptions } from './commands/keyspace/expire';
 import * as CommandDel from './commands/keyspace/del';
@@ -115,6 +117,22 @@ export class RedisXTransaction<L extends any[] = [
 	], F, U>;
 	ZRANGE(...args: unknown[]) {
 		return this._addModuleCommand(CommandZrange, args);
+	}
+	/**
+	 * This command works exactly like EXPIRE but the time to live of the key is specified in milliseconds instead of seconds.
+	 * - Available since: 2.6.0.
+	 * - Time complexity: O(1).
+	 * @param key Key to get.
+	 * @param seconds Time to live in seconds.
+	 * @param options Options. See ExpireOptionsJsdoc.
+	 * @returns Returns `1` if the timeout was set. Returns `0` if the timeout was not set; for example, the key doesn't exist, or the operation was skipped because of the provided arguments.
+	 */
+	PEXPIRE(key: string, seconds: number, options?: PexpireOptions): RedisXTransaction<[
+		...L,
+		0 | 1
+	], F, U>;
+	PEXPIRE(...args: unknown[]) {
+		return this._addModuleCommand(CommandPexpire, args);
 	}
 	/**
 	 * Set a timeout on key.
