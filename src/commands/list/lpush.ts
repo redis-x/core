1128768eaef5717e76515f2447470e23b9ab2bbe
Daniil Kirichenko <daniil@kirick.me>
2024-07-25 18:13:16 +0300

    Formatting fix

--- src/commands/list/lpush.ts
+++ src/commands/list/lpush.ts
@@ -11,16 +11,16 @@ import { InputReturnType } from '../../types';
  * @param elements -
  * @returns -
  */
- export function input(key: string, ...elements: string[]): InputReturnType;
+export function input(key: string, ...elements: string[]): InputReturnType;
  /**
   * @param key -
   * @param elements -
   * @returns -
   */
- export function input(key: string, elements: string[] | Set<string>): InputReturnType;
+export function input(key: string, elements: string[] | Set<string>): InputReturnType;
  // eslint-disable-next-line jsdoc/require-jsdoc
- export function input(key: string, arg1: string | string[] | Set<string>, ...args: string[]): InputReturnType {
- 	let elements: string[] = [];
+export function input(key: string, arg1: string | string[] | Set<string>, ...args: string[]): InputReturnType {
+	let elements: string[] = [];
 	if (typeof arg1 === 'string') {
 		elements = args;
 		elements.unshift(arg1);
